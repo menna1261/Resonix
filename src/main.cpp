0c73beff414ef6c45da35bffc0574dd9af1dc302
Menna Jaheen <menna.jaheen16@gmail.com>
2026-02-26 19:47:52 +0200

    Playing the first audio yay

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include<SDL2/SDL.h>
 #include<iostream>
+
 int main(){
 
     SDL_AudioSpec spec;
@@ -16,6 +17,21 @@ int main(){
     else{
         std::cerr<<"audio loaded sucessfully\n";
     }
+
+    SDL_AudioDeviceID dev = SDL_OpenAudioDevice(NULL, 0,&spec , &spec, 0);
+    if(dev == 0){
+        std::cout<<"Failed to open file\n";
+    }
+    else{
+        std::cout<<"opened file\n";
+
+    }
+
+    SDL_QueueAudio(dev, audio_buf, audio_len);
+    SDL_FreeWAV(audio_buf);          
+    SDL_PauseAudioDevice(dev, 0);    
+    SDL_Delay(5000);
+    SDL_CloseAudioDevice(dev);
 
     return 0;
 }
