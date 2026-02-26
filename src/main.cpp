123c17685db2e75beda0086df37933b07bab484c
Menna Jaheen <menna.jaheen16@gmail.com>
2026-02-26 18:57:21 +0200

    loaded audio sucessfully

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include<SDL2/SDL.h>
-
+#include<iostream>
 int main(){
 
+    SDL_AudioSpec spec;
+    Uint8* audio_buf;
+    Uint32 audio_len;
+
     SDL_InitSubSystem(SDL_INIT_AUDIO);
+    
+
+    SDL_AudioSpec* sound = SDL_LoadWAV("mario.wav", &spec , &audio_buf, &audio_len);
+    if (sound == NULL){
+        std::cout<<"failed to load .wav file\n";
+    }
+    else{
+        std::cerr<<"audio loaded sucessfully\n";
+    }
+
     return 0;
 }
